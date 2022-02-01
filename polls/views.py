149d003d625b4e8eecb1c6783600b49ec7b92d99
saqibmahmood <saqib.x86@gmail.com>
2022-02-02 02:11:51 +0300

    v0.4

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,29 @@
-from logging import lastResort
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+'''
 def index (request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
@@ -25,6 +44,7 @@ def results(request, question_id):
         'question':question
         }
     )
+'''
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
